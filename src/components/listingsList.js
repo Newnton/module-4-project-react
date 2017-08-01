108d6f62b36fa9985cfec4e8feeeca7f300912c9
Zach Dubno <zachedubno@gmail.com>
2017-08-01 09:17:09 -0400

    cleaned up some commented out code and console logs

--- src/components/listingsList.js
+++ src/components/listingsList.js
@@ -3,7 +3,6 @@ import { Card } from 'semantic-ui-react'
 import Listing from './listing'
 
 const ListingsList = ({listings}) => {
-  console.log(listings)
   return(
     <Card.Group itemsPerRow={3}>
       {listings !== null ? listings.map((listing) => <Listing listing={listing} />) : null}
